a1f323951e077d264117f32be98b095e74611dbb
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-07-15 17:21:37 -0400

    using priceline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "pcln-design-system";
 
 import "./styles.css";
 
 function App() {
-  return <h1>hello sky</h1>;
+  return (
+    <ThemeProvider>
+      <h1>hello sky</h1>
+    </ThemeProvider>
+  );
 }
 
 const rootElement = document.getElementById("root");
