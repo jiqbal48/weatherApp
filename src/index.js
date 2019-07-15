63c18c6881d99fbb481c57a94dbbf7dacd93c845
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-07-15 17:15:05 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return <h1>hello sky</h1>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
