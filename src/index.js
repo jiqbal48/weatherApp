686c64649ff02e344e866e98c75b6745e475fe22
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-07-15 21:26:03 -0400

    some box with text

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "pcln-design-system";
+import { ThemeProvider, Box, Text } from "pcln-design-system";
 
 import "./styles.css";
 
 function App() {
   return (
     <ThemeProvider>
-      <h1>hello sky</h1>
+      <Box p={3} bg="#c0cad5" color="#007aff">
+        <Text>hello sky</Text>
+      </Box>
     </ThemeProvider>
   );
 }
